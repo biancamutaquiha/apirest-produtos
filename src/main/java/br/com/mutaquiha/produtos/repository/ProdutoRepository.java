6090c3a298aa2f9a553c0495bc8b72043c41362b
Bianca Mutaquiha <biafersil@gmail.com>
2020-04-14 17:20:40 -0300

    repository add

---
+++ src/main/java/br/com/mutaquiha/produtos/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.mutaquiha.produtos.repository;
+
+import br.com.mutaquiha.produtos.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
